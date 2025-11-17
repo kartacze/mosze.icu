dc09a2b3c85b19fdfda0f41d9b959909e6d0b18c
Teodor Pytka <teodoreczek@gmail.com>
2025-11-17 10:31:06 +0100

    feat: correct website response

--- modules/website.nix
+++ modules/website.nix
@@ -29,40 +29,20 @@
 
         locations."/" = {
           # proxyPass = "http://127.0.0.1:4000";
-          extraConfig = ''
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-          '';
-          return = "200 '<html><body>It works</body></html>'";
           # extraConfig = ''
-          #   default_type text/html;
-          #   proxy_ssl_server_name on;
-          #   proxy_pass_header Authorization;
+          #   proxy_set_header Host $host;
+          #   proxy_set_header X-Real-IP $remote_addr;
+          #   proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          #   proxy_set_header X-Forwarded-Proto $scheme;
           # '';
+          return = "200 '<html><body>It works</body></html>'";
+          extraConfig = ''
+            default_type text/html;
+            proxy_ssl_server_name on;
+            proxy_pass_header Authorization;
+          '';
         };
       };
     };
-
-    # Make sure the "website" user has access to /srv/website
-    # systemd.tmpfiles.rules = [ "d /srv/website 0750 website nginx" ];
-
-    # Make sure acme module is active for the "kyren.codes" ssl cert
-
-    # services.nginx.enable = true;
-    # services.nginx.virtualHosts."kyren.codes" = {
-    #   useACMEHost = "kyren.codes";
-    #   forceSSL = true;
-    #   locations."/" = {
-    #     index = "index.html";
-    #     root = "/srv/website";
-    #   };
-    #
-    #   locations."/404.html" = { root = "/srv/website"; };
-    #   extraConfig = ''
-    #     error_page 404 /404.html;
-    #   '';
-    # };
   };
 }
